5bd24b083c84d0bf9ef47658b9b612f8f8ea433e
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2021-02-01 14:47:45 +0100

    La till bilder i informationen

--- filmer.js
+++ filmer.js
@@ -9,9 +9,8 @@ function showJsonAsHtml(filmer, wantedTitle) {
 
   filmer.filter(film => film.title === wantedTitle)
     .forEach(film => {
-      $film.append(`<div> <h3 id="titleName">${film.title}</h3> <p>Handling: ${film.description}</p> <p>Regissör: ${film.director}</p> <p>Skådespelare: ${film.actors}</p> </div>`)
+      $film.append(`<div id="movieText"> <h3 id="titleName">${film.title}</h3> <p>Handling: ${film.description}</p> <p>Regissör: ${film.director}</p> <p>Skådespelare: ${film.actors}</p> </div> <div id="moviePicture"><img src="${film.images}" id="moviePic"></div>`)
     })
-  $('#test').hide();
-  $('.secondcolumn').html($film);
-
+  $('.secondcolumn').replaceWith($film);
+  // $('.secondcolumn').html($film);
 };

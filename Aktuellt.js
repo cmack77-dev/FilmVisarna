a5e2f00e3025a3723598aa1df88acda80ea133de
Adnan Kovacevic <69817904+Ado-wq@users.noreply.github.com>
2021-02-02 14:40:45 +0100

    Minor changes
    Minor changes in code to make it work, and move it to left side of columns.

--- Aktuellt.js
+++ Aktuellt.js
@@ -1,21 +1,25 @@
 async function readJsonFilmer(title) {
-  let = aktuellt = await $getJSON('JSON-filer/filmer.json');
+  let aktuellt = await $.getJSON('JSON-filer/filmer.json');
   console.log(aktuellt);
   showJsonasHtml(aktuellt);
 }
 
+
 function showJsonasHtml(aktuellt) {
-  for (let aktuellt of aktuella) {
+  for (let aktuella of aktuellt) {
     let $aktuella = $('<div class="firstcolumn"></div>');
 
     for (let i in aktuella) {
       let value = aktuella[i];
-      if (i === 'titel') { value = value.join(', '); }
-      $aktuella.append('<div><p>' + i + ': </p>' + value + '</div>');
+      if (i === 'title')
+        $aktuella.append('<div><p class="movieClick" onclick="readJson(' + "'" + value + "'" + '); "> ' + value + '</div>');
 
     }
-    $('body').append($aktuella);
+
+    $('main').prepend($aktuella);
+
   }
+
 }
 
-readJsonFilmer()
+readJsonFilmer();

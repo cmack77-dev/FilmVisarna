bd17b5c8bdb1ff5f12d7c99319d8b3b8d5319f3b
Adnan Kovacevic <69817904+Ado-wq@users.noreply.github.com>
2021-02-04 14:55:54 +0100

    Added function for tickets
    added a dropdownlist for choosing tickets,

--- Biljetttyp.js
+++ Biljetttyp.js
@@ -1,6 +1,34 @@
-@import url(SeatsbookingView.js);
 
-fucktion ticketType(){
-  showSeats(showSeats);
-  console.log(showSeats);
+
+async function getSeats(seatPerRow) {
+  let seats = await $.getJSON('JSON-filer/salonger.json');
+  console.log(seats)
+  renderSeats(seats)
 }
+$(document).ready(function () {
+  $('#Välj').click(function () {
+
+    var values = ["Normal", "Barn", "Pensionär",];
+
+    $('#container')
+      .append(
+        $(document.createElement('label')).prop({
+          for: 'Biljett'
+        }).html('Välj antal vuxen biljetter: ')
+      )
+      .append(
+        $(document.createElement('select')).prop({
+          id: 'Biljetter',
+          name: 'Biljetter'
+        })
+      )
+
+    for (const val of values) {
+      $('#Biljetter').append($(document.createElement('option')).prop({
+        value: val,
+        text: val.charAt(0).toUpperCase() + val.slice(1)
+      }))
+    }
+  })
+});
+$('.thirdcolumn').append($())

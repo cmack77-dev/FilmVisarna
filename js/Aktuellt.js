b8d5b577b5b0461ce44a9376f8d50e18cd078611
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2021-02-09 15:51:56 +0100

    Started styling on "aktuella filmer" and navbar

--- js/Aktuellt.js
+++ js/Aktuellt.js
@@ -8,7 +8,7 @@ function showJsonasHtml(aktuellt) {
   aktuellt.filter(aktuella => aktuella.title)
     .forEach(aktuella => {
       $aktuella.append(
-        '<div><p class="movieClick" onclick="readJson(' + "'" + aktuella.title + "'" + '); "> <img src= "' + aktuella.images + '" id="moviePictureLink" </div>'
+        '<div class="movieFrame"><p class="movieClick" onclick="readJson(' + "'" + aktuella.title + "'" + '); "> <img src= "' + aktuella.images + '" id="moviePictureLink"><button id="moviePictureButton">Hitta biljetter</button></p></div>'
       )
     })
   $('.firstcolumn').append($aktuella)

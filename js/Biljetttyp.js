a48892c0ce5c6ce18f7d7b4482174bfc5aee3db7
Adnan Kovacevic <69817904+Ado-wq@users.noreply.github.com>
2021-02-10 09:44:57 +0100

    Dropdown
    added funktion for dropdown
    made a JSON file for choosing quantity
    
    added div and select into HTML

--- js/Biljetttyp.js
+++ js/Biljetttyp.js
@@ -1,6 +1,20 @@
-// @import url(SeatsbookingView.js);
 
-// fucktion ticketType(){
-//   showSeats(showSeats);
-//   console.log(showSeats);
-// }
+async function dropdown(dropdown) {
+  let dropdown = $('#ticket-dropdown');
+  dropdown.empty();
+  dropdown.append('<option selected="true" disabled>Välj antal bijetter</option>');
+  dropdown.prop('selectedIndex', 0);
+  let ticketType = await $.getJSON('JSON-filer/ticketType.json')
+  let dropdown = $('#ticket-dropdown');
+  dropdown.empty();
+  dropdown.append('<option selected="true" disabled>Välj antal bijetter</option>');
+  dropdown.prop('selectedIndex', 0);
+
+
+  $.getJSON(ticketType, function (data) {
+    $.each(data, function (entry) {
+      dropdown.append($('<option></option>').attr('value', entry.Antal).text(entry.TicketType));
+    })
+    $('.ticket-dropdown').append()
+  });
+}

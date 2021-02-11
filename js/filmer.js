ed44174ff0a51139a0e5f95e945ea94f5cb4cd27
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2021-02-11 01:02:19 +0100

    worked on the trailer display

--- js/filmer.js
+++ js/filmer.js
@@ -11,9 +11,17 @@ function showJsonAsHtml(filmer, wantedTitle) {
     .filter(film => film.title === wantedTitle)
     .forEach(film => {
       $film.append(
-        `<section class="middleColumnChange"><div id="movieText"> <h3 id="titleName">${film.title}</h3> <p>Handling: ${film.description}</p> <p>Regissör: ${film.director}</p> <p>Skådespelare: ${film.actors}</p> </div> <div id="moviePicture"><img src="${film.images}" id="moviePic"></div> <div> <iframe id="trailer" src="${film.youtubeTrailers}" allowfullscreen></iframe> </div></section>`
+        `<section class="middleColumnChange"><div class="moviePicture"><img src="${film.images}" id="moviePic"></div> <button id="trailer-button" type="button">Play trailer</button> <div class="movieText"> <h3 id="titleName">${film.title}</h3> <p>Handling: ${film.description}</p> <p>Regissör: ${film.director}</p> <p>Skådespelare: ${film.actors}</p></div> </section>`
       )
+
       //   $('.secondcolumn').replaceWith($film)
       $('.secondcolumn').html($film);
+      let $trailer = `<div class="trailer-container"><iframe id="trailer" src="${film.youtubeTrailers}" allowfullscreen></iframe><button id="close-button">&times;</button></div>`;
+
+      // Funktion för att byta ut bilden mot en trailer.  EJ FÄRDIG!!!!
+      $('body').on('click', '#trailer-button', () => {
+        $('#moviePic').replaceWith($trailer);
+      })
     })
+
 }

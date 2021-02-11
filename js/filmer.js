6ae3a1efeeb07135f933504ff1a9d61320347e55
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2021-02-11 16:47:51 +0100

    started on pop-up trailer

--- js/filmer.js
+++ js/filmer.js
@@ -3,6 +3,15 @@ async function readJson(title) {
   showJsonAsHtml(filmer, title)
   getShows(title) //Markus har lagt in för att ropa på funktioner för bokning etc.
 }
+function toggle() {
+  let trailer = document.querySelector('.trailer-container')
+  trailer.classList.toggle('active')
+
+  $('#close-button').on('click', () => {
+    $('#trailer')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+  })
+
+}
 
 function showJsonAsHtml(filmer, wantedTitle) {
   let $film = $('<div class="moviecolumn"></div>')
@@ -11,17 +20,16 @@ function showJsonAsHtml(filmer, wantedTitle) {
     .filter(film => film.title === wantedTitle)
     .forEach(film => {
       $film.append(
-        `<section class="middleColumnChange"><div class="moviePicture"><img src="${film.images}" id="moviePic"></div> <button id="trailer-button" type="button">Play trailer</button> <div class="movieText"> <h3 id="titleName">${film.title}</h3> <p>Handling: ${film.description}</p> <p>Regissör: ${film.director}</p> <p>Skådespelare: ${film.actors}</p></div> </section>`
+        `<section class="middleColumnChange"><div class="moviePicture"><img src="${film.images}" id="moviePic"></div> <button onclick="toggle();" id="trailer-button"  type="button">Play trailer</button> <div class="movieText"> <h3 id="titleName">${film.title}</h3> <p>Handling: ${film.description}</p> <p>Regissör: ${film.director}</p> <p>Skådespelare: ${film.actors}</p></div> <div class="trailer-container"><iframe id="trailer" src="${film.youtubeTrailers}" allowfullscreen="true" allowscriptaccess="always"></iframe><button onclick="toggle();" id="close-button">&times;</button></div> </section>`
       )
-
       //   $('.secondcolumn').replaceWith($film)
       $('.secondcolumn').html($film);
-      let $trailer = `<div class="trailer-container"><iframe id="trailer" src="${film.youtubeTrailers}" allowfullscreen></iframe><button id="close-button">&times;</button></div>`;
 
-      // Funktion för att byta ut bilden mot en trailer.  EJ FÄRDIG!!!!
-      $('body').on('click', '#trailer-button', () => {
-        $('#moviePic').replaceWith($trailer);
-      })
+      // let $trailer = `<div class="trailer-container"><iframe id="trailer" src="${film.youtubeTrailers}" allowfullscreen></iframe><button id="close-button">&times;</button></div>`;  
+
+      // $('body').on('click', '#trailer-button', () => {
+      //   $('#moviePic').replaceWith($trailer);
+      // })
     })
 
 }

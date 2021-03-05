52298f6b7160d799e91f5c84e9618c5853ee5ee5
cmack77-dev <67069292+cmack77-dev@users.noreply.github.com>
2021-03-05 10:47:30 +0100

    changed to global variables for login and minor small fixes

--- www/js/login-registration.js
+++ www/js/login-registration.js
@@ -1,88 +1,67 @@
-let realname;
-let email;
-let uname;
-let pwd;
-let cpwd;
+// let realname
+// let email
+// let uname
+// let pwd
+// let cpwd
 
+function registration () {
+  storage.realname = $('#t1').val()
+  storage.email = $('#t2').val()
+  storage.uname = $('#t3').val()
+  storage.pwd = $('#t4').val()
+  storage.cpwd = $('#t5').val()
 
-function registration() {
+  console.log(storage.realname)
+  console.log(storage.email)
+  console.log(storage.uname)
+  console.log(storage.pwd)
+  console.log(storage.cpwd)
 
-  realname = $('#t1').val();
-  email = $('#t2').val();
-  uname = $('#t3').val();
-  pwd = $('#t4').val();
-  cpwd = $('#t5').val();
-
-
-
-  console.log(realname)
-  console.log(email)
-  console.log(uname)
-  console.log(pwd)
-  console.log(cpwd)
-
-
-  // val av Special tecken för PW kan läggas till senare| (?=.*?[#?!@$%^&*-]) | 
-  let pwd_expression = /^(?=.*?[Z])(?=.*?[a-z])(?=.*?[0-9])/;
-  let letters = /^[A-Za-z]+$/;
-  let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  // val av Special tecken för PW kan läggas till senare| (?=.*?[#?!@$%^&*-]) |
+  let pwd_expression = /^(?=.*?[Z])(?=.*?[a-z])(?=.*?[0-9])/
+  let letters = /^[A-Za-z]+$/
+  let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
   //if stats för att vägleda användare att skriva in rätt i formulär
-  if (realname == '') {
-    alert('Vänligen skriv ditt namn');
-  }
-  else if (!letters.test(realname)) {
-    alert('endast vanliga tecken i namn');
-  }
-  else if (email == '') {
-    alert('skirv en E-post adress');
-  }
-  else if (!filter.test(email)) {
-    alert('Ogiltig E-post');
-  }
-  else if (uname == '') {
-    alert('Ange användarnamn.');
-  }
-  else if (!letters.test(uname)) {
-    alert('Användarnamn får endast innehålla vanliga tecken');
-  }
-  else if (pwd == '') {
-    alert('Vänligen skriv in lösenord');
-  }
-  else if (cpwd == '') {
-    alert('Bekräfta lösenord');
-  }
-  else if (!pwd_expression.test(pwd)) {
-    alert('Lösenord måste innehålla små och stora bokstäver och minst 1 siffra');
-  }
-  else if (pwd != cpwd) {
-    alert('Lösenord matchar inte, var god försök igen!');
-  }
-  else if ($('#t5').val().length < 6) {
-    alert('Minst 6 tecken i lösenordet');
-  }
+  if (storage.realname == '') {
+    alert('Vänligen skriv ditt namn')
+  } else if (!letters.test(storage.realname)) {
+    alert('endast vanliga tecken i namn')
+  } else if (storage.email == '') {
+    alert('skriv en E-post adress')
+  } else if (!filter.test(storage.email)) {
+    alert('Ogiltig E-post')
+  } else if (storage.uname == '') {
+    alert('Ange användarnamn.')
+  } else if (!letters.test(storage.uname)) {
+    alert('Användarnamn får endast innehålla vanliga tecken')
+  } else if (storage.pwd == '') {
+    alert('Vänligen skriv in lösenord')
+  } else if (storage.cpwd == '') {
+    alert('Bekräfta lösenord')
+  } else if (!pwd_expression.test(storage.pwd)) {
+    alert('Lösenord måste innehålla små och stora bokstäver och minst 1 siffra')
+  } else if (storage.pwd != storage.cpwd) {
+    alert('Lösenord matchar inte, var god försök igen!')
+  } else if ($('#t5').val().length < 6) {
+    alert('Minst 6 tecken i lösenordet')
+  } else {
+    alert('Tack för din registrering!')
 
-  else {
-    alert('Tack för din registrering!');
-
-    window.location = "#start";
+    window.location = '#start'
   }
 }
 
 //Reset knapp för att rensa förmulär
-function clearFunc() {
-  $('#t1').val('');
-  $('#t2').val('');
-  $('#t3').val('');
-  $('#t4').val('');
-  $('#t5').val('');
-
+function clearFunc () {
+  $('#t1').val('')
+  $('#t2').val('')
+  $('#t3').val('')
+  $('#t4').val('')
+  $('#t5').val('')
 }
-function createAcount() {
-  $('#submit').trigger('click'), function () {
-    formID = $(this).value
-  }
-
+function createAcount () {
+  $('#submit').trigger('click'),
+    function () {
+      formID = $(this).value
+    }
 }
-
-
-
--- www/js/storage.js
+++ www/js/storage.js
@@ -1,11 +1,29 @@
 let storage = {
-  selectedMovie: "",
+  selectedMovie: '',
   movies: [],
-  vuxna:0,
-  pensionar:0,
-  barn:0
+  vuxna: 0,
+  pensionar: 0,
+  barn: 0,
+  realname: '',
+  email: '',
+  uname: '',
+  pwd: '',
+  cpwd: ''
 }
-async function initStorage() {
-  storage.movies = await getMovies();
+async function initStorage () {
+  storage.movies = await getMovies()
 }
-initStorage();
+initStorage()
+
+
+else if (uname == '') {
+    $('#t3').after('<p class="formmsg">Ange Användarnamn*</p>');
+  }
+  else if (!letters.test(uname)) {
+    replace
+    $('#t3').after('<p class="formmsg">Användarnamn får endast innehålla vanliga tecken*</p>');else if (uname == '') {
+    $('#t3').after('<p class="formmsg">Ange Användarnamn*</p>');
+  }
+  else if (!letters.test(uname)) {
+    replace
+    $('#t3').after('<p class="formmsg">Användarnamn får endast innehålla vanliga tecken*</p>');

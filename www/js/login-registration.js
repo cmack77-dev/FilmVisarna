efa5404a47ebce3a7ca5d3b0f81bee55a2dc4eff
Adnan Kovacevic <69817904+Ado-wq@users.noreply.github.com>
2021-03-06 10:16:42 +0100

    rego from update,
    warning message now clears when new warning comes,  input box is now same size in bigger messages, and work on smaller devices,

--- www/js/login-registration.js
+++ www/js/login-registration.js
@@ -5,7 +5,7 @@ window.location = '#start'
 // let pwd;
 // let cpwd;
 
-function registration () {
+function registration() {
   storage.realname = $('#t1').val()
   storage.email = $('#t2').val()
   storage.uname = $('#t3').val()
@@ -20,38 +20,50 @@ function registration () {
 
   // val av Special tecken för PW kan läggas till senare| (?=.*?[#?!@$%^&*-]) |
   let pwd_expression = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])/
-  let letters = /^[A-Za-z \båäö]+$/
+  let letters = /^[A-Za-z0-9 \båäö]+$/
   let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
   //if stats för att vägleda användare att skriva in rätt i formulär
   if (storage.realname == '') {
+    $('.formmsg').replaceWith('')
     $('#t1').after('<p class="formmsg">Vänligen skriv ditt Namn*</p>')
-  } else if (!letters.test(storage.realname)) {
+  }
+  else if (!letters.test(storage.realname)) {
+    $('.formmsg').replaceWith('')
     $('#t1').after('<p class="formmsg">endast vanliga tecken i Namn*</p>')
+
   } else if (storage.email == '') {
+    $('.formmsg').replaceWith('')
     $('#t2').after('<p class="formmsg">skriv en E-post*</p>')
+
   } else if (!filter.test(storage.email)) {
+    $('.formmsg').replaceWith('')
     $('#t2').after('<p class="formmsg">Ogiltig E-post*</p>')
   } else if (storage.uname == '') {
+    $('.formmsg').replaceWith('')
     $('#t3').after('<p class="formmsg">Ange Användarnamn*</p>')
   } else if (!letters.test(storage.uname)) {
     $('.formmsg').replaceWith('')
     $('#t3').after(
-      '<p class="formmsg">Användarnamn får endast innehålla vanliga tecken*</p>'
-    )
+      '<p class="formmsg">Användarnamn får endast innehålla vanliga tecken*</p>')
   } else if (storage.pwd == '') {
-    $('#t4').after('<p class="Vänligen skriv ett lösenord*</p>')
+    $('.formmsg').replaceWith('')
+    $('#t4').after('<p class="formmsg">Vänligen skriv ett lösenord*</p>')
   } else if (storage.cpwd == '') {
+    $('.formmsg').replaceWith('')
     $('#t5').after('<p class="formmsg">Bekräfta lösenord*</p>')
   } else if (!pwd_expression.test(storage.pwd)) {
-    $('#5').after(
+    $('.formmsg').replaceWith('')
+    $('#t4').after(
       '<p class="formmsg">Lösenord måste innehålla små och stora bokstäver och minst 1 siffra*</p>'
     )
   } else if (storage.pwd != storage.cpwd) {
+    $('.formmsg').replaceWith('')
     $('#t5').after(
-      '<p class="formmsg">Lösenord matchar inte, var god försök igen!*</p>'
-    )
+      '<p class="formmsg">Lösenord matchar inte, var god försök igen!*</p>')
+
   } else if ($('#t5').val().length < 6) {
-    $('#t1').after('<p class="formmsg">Minst 6 tecken i lösenordet</p>')
+    $('.formmsg').replaceWith('')
+    $('#t5').after('<p class="formmsg">Minst 6 tecken i lösenordet</p>')
   } else {
     alert('Tack för din registrering!')
 
@@ -60,14 +72,16 @@ function registration () {
 }
 
 //Reset knapp för att rensa förmulär
-function clearFunc () {
+function clearFunc() {
+  $('.formmsg').replaceWith('')
   $('#t1').val('')
   $('#t2').val('')
   $('#t3').val('')
   $('#t4').val('')
   $('#t5').val('')
 }
-function createAcount () {
+
+function createAcount() {
   $('#submit').trigger('click'),
     function () {
       formID = $(this).value
